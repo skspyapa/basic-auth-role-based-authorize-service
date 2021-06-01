d6738322086c21331e2f1ef2a821e5ab94c21708
skspyapa <supun@rumex.lk>
2021-06-02 00:21:41 +0530

    Add method level security for the APIs

--- src/main/java/lk/sky360solutions/authentication/config/WebSecurityConfig.java
+++ src/main/java/lk/sky360solutions/authentication/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final UserRepository userRepository;
@@ -52,10 +54,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.cors().and().csrf().disable()
       .authorizeRequests()
-      .antMatchers("/products/findAll").hasAnyAuthority("USER", "CREATOR", "EDITOR", "ADMIN")
-      .antMatchers("/products/save").hasAnyAuthority("ADMIN", "CREATOR")
-      .antMatchers("/products/edit/**").hasAnyAuthority("ADMIN", "EDITOR")
-      .antMatchers("/products/delete/**").hasAuthority("ADMIN")
       .anyRequest().authenticated()
       .and()
       .httpBasic().and()
--- src/main/java/lk/sky360solutions/authentication/controller/ProductController.java
+++ src/main/java/lk/sky360solutions/authentication/controller/ProductController.java
@@ -5,6 +5,7 @@ import lk.sky360solutions.authentication.model.Product;
 import lk.sky360solutions.authentication.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,19 +16,24 @@ import java.util.List;
 public class ProductController {
 
 	private final ProductService productService;
-
+/*
+* role name should start with ROLE_ prefix
+* */
+	@Secured({"ROLE_USER", "ROLE_CREATOR", "ROLE_EDITOR", "ROLE_ADMIN"})
 	@ResponseStatus(HttpStatus.OK)
   @GetMapping(value = "findAll")
 	public List<Product> viewAllProduct() {
 		return productService.listAll();
 	}
 
+	@Secured({"ROLE_ADMIN", "ROLE_CREATOR"})
   @ResponseStatus(HttpStatus.CREATED)
 	@PostMapping(value = "save")
 	public Product saveProduct(@RequestBody Product product) {
 		return productService.save(product);
 	}
 
+	@Secured({"ROLE_ADMIN", "ROLE_EDITOR"})
   @ResponseStatus(HttpStatus.OK)
 	@PutMapping(value = "edit/{id}")
 	public Product editProduct(@PathVariable(name = "id") Long id,
@@ -35,6 +41,7 @@ public class ProductController {
 		return productService.update(product, id);
 	}
 
+	@Secured("ROLE_ADMIN")
   @ResponseStatus(HttpStatus.OK)
 	@DeleteMapping(value = "delete/{id}")
 	public void deleteProduct(@PathVariable(name = "id") Long id) throws ProductNotFoundException {
